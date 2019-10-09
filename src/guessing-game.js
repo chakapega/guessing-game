fe1b5c0aba7249613e036ac14b6b45abce2217a0
Vadim <chakapega@gmail.com>
2019-10-10 01:28:14 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    };
 
     setRange(min, max) {
-
-    }
+        this.minimumRangeValue = min;
+        this.maximumRangeValue = max;
+    };
 
     guess() {
+        this.arithmeticalMean = Math.ceil((this.minimumRangeValue + this.maximumRangeValue) / 2);
 
-    }
+        return this.arithmeticalMean;
+    };
 
     lower() {
-
-    }
+        this.maximumRangeValue = this.arithmeticalMean;
+    };
 
     greater() {
-
-    }
-}
+        this.minimumRangeValue = this.arithmeticalMean;
+    };
+};
 
 module.exports = GuessingGame;
